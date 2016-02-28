55892fa108568b1940b50473543834779813c491
N@thix86 <nathan.page86@gmail.com>
2016-02-28 22:44:31 +0100

    Improve quota showing.

--- src/AppBundle/Controller/SharedFileController.php
+++ src/AppBundle/Controller/SharedFileController.php
@@ -13,7 +13,8 @@ use Symfony\Component\Finder\Finder;
 class SharedFileController extends Controller
 {
     const DIRECTORY_NAME = "/../web/shared_files";
-    const DIRECTORY_MAX_SIZE = 10000;
+    const DIRECTORY_MAX_SIZE_NUMBER = 5000;
+    const DIRECTORY_MAX_SIZE_TEXT = '5 Go';
 
     /**
      * @Route("/create", name="admin_shared_file_create")
@@ -50,9 +51,6 @@ class SharedFileController extends Controller
         $path = $this->get('kernel')->getRootDir() . self::DIRECTORY_NAME;
         $exception = null;
         $fs = new Filesystem();
-        $finder = new Finder();
-        $size = 0;
-        $class = 'success';
 
         if(!$fs->exists($path)) {
             try {
@@ -62,31 +60,42 @@ class SharedFileController extends Controller
             }
         }
 
-        if($exception === null){
-            foreach($finder->files()->in($path) as $file){
-                $size += $this->getFileSize($path . '/' . $file->getRelativePathname());
-            }
-        }
-
-        $percent = $this->getPercent($size);
-
-        if($percent >= 60 && $percent <= 80){
-            $class = 'warning';
-        } elseif($percent > 80) {
-            $class = 'danger';
-        }
+        $bytes = $exception !== null ? 0 : $this->getDirectorySize($path);
+        $sizeInMo = $this->formatSize($bytes, 'Mo');
+        $percent = $this->getUsePercentage($sizeInMo);
+        $class = $this->getProbressBarClass($percent);
 
         return $this->render('@App/Admin/SharedFile/quota.html.twig', array(
             'exception' => $exception,
-            'size' => $size,
-            'maxSize' => self::DIRECTORY_MAX_SIZE,
+            'sizeInMo' => $sizeInMo,
+            'formattedSize' => $this->formatSize($bytes, 'auto', true),
+            'maxSizeText' => self::DIRECTORY_MAX_SIZE_TEXT,
+            'maxSizeNumber' => self::DIRECTORY_MAX_SIZE_NUMBER,
             'percent' => $percent,
             'class' => $class
         ));
     }
 
     /**
-     * Format file size in GB.
+     * Get directory size in bytes.
+     *
+     * @param $path
+     * @param int $size
+     * @return float|int
+     */
+    private function getDirectorySize($path, $size = 0)
+    {
+        $finder = new Finder();
+
+        foreach($finder->files()->in($path) as $file){
+            $size += $this->getFileSize($path . '/' . $file->getRelativePathname());
+        }
+
+        return $size;
+    }
+
+    /**
+     * Get file size in bytes.
      *
      * @param $size
      * @param int $precision
@@ -94,13 +103,72 @@ class SharedFileController extends Controller
      */
     private function getFileSize($filename)
     {
-        $sizeInGb = (filesize($filename) * .0009765625) * .0009765625; // bytes to MB
-
-        return $sizeInGb >= 1 ? round($sizeInGb, 1) : 0;
+        return filesize($filename);
     }
 
-    private function getPercent($size)
+    /**
+     * Get use percentage of shared files directory.
+     * The size parameter must be in Mo.
+     *
+     * @param $size
+     * @return float
+     */
+    private function getUsePercentage($size)
     {
-        return round(($size * 100)/self::DIRECTORY_MAX_SIZE, 0);
+        return round(($size * 100)/self::DIRECTORY_MAX_SIZE_NUMBER, 0);
+    }
+
+    /**
+     * Get progressbar class.
+     *
+     * @param $percent
+     * @return string
+     */
+    private function getProbressBarClass($percent)
+    {
+        $class = 'success';
+
+        if($percent >= 60 && $percent <= 80){
+            $class = 'warning';
+        } elseif($percent > 80) {
+            $class = 'danger';
+        }
+
+        return $class;
+    }
+
+    /**
+     * Get formatted size.
+     *
+     * @param $bytes
+     * @param string $format
+     * @param bool|false $suffixed
+     * @return float|int|string
+     */
+    private function formatSize($bytes, $format = 'auto', $suffixed = false)
+    {
+        $formattedSize = 0;
+        $suffix = '';
+        $formats = array(
+            'Go' => 1073741824,
+            'Mo' => 1048576,
+            'Ko' => 1024,
+            'o' => 1
+        );
+
+        if($format != 'auto' && array_key_exists($format, $formats)){
+            $formattedSize = round($bytes / $formats[$format] * 100) / 100;
+            $suffix = $format;
+        } else {
+            foreach($formats as $k => $v){
+                if($bytes >= $v){
+                    $formattedSize = round($bytes / $v * 100) / 100;
+                    $suffix = $k;
+                    break;
+                }
+            }
+        }
+
+        return $suffixed ? $formattedSize . ' ' . $suffix : $formattedSize;
     }
 }
